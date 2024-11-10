8527e696e0a6442a9aecb9f3be27dc1ac89c8e68
Sergio Fernández <sergiofdezfernandez@gmail.com>
2024-11-10 17:43:52 +0100

    style: styling topbar

--- src/app/core/components/topbar/topbar.component.ts
+++ src/app/core/components/topbar/topbar.component.ts
@@ -1,13 +1,37 @@
-import { Component } from '@angular/core';
+import { DatePipe } from '@angular/common';
+import { Component, OnInit } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 import { ButtonModule } from 'primeng/button';
 import { RippleModule } from 'primeng/ripple';
+import { ToggleButtonModule } from 'primeng/togglebutton';
 import { ToolbarModule } from 'primeng/toolbar';
+import { interval } from 'rxjs';
 
 @Component({
   selector: 'dms-topbar',
   standalone: true,
-  imports: [ToolbarModule, ButtonModule, RippleModule],
+  imports: [ToolbarModule, ButtonModule, RippleModule, ToggleButtonModule, FormsModule, DatePipe],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.css',
 })
-export class TopbarComponent {}
+export class TopbarComponent implements OnInit {
+
+  isOnline = true;
+  currentDate: Date;
+  utc: string;
+
+  constructor() {
+    this.currentDate = new Date();
+    this.utc = this.currentDate.toUTCString();
+  }
+
+  ngOnInit() {
+    interval(1000).subscribe(() => {
+      const now = new Date();
+      this.currentDate = now;
+    });
+  }
+  signOut() {
+    throw new Error('Method not implemented.');
+  }
+}
