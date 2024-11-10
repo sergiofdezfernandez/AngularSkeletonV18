7dead1c4826e82d09150e321e001a0335807ea36
Sergio Fernández <sergiofdezfernandez@gmail.com>
2024-11-10 17:48:47 +0100

    refactor

--- src/app/core/components/topbar/topbar.component.ts
+++ src/app/core/components/topbar/topbar.component.ts
@@ -10,12 +10,18 @@ import { interval } from 'rxjs';
 @Component({
   selector: 'dms-topbar',
   standalone: true,
-  imports: [ToolbarModule, ButtonModule, RippleModule, ToggleButtonModule, FormsModule, DatePipe],
+  imports: [
+    ToolbarModule,
+    ButtonModule,
+    RippleModule,
+    ToggleButtonModule,
+    FormsModule,
+    DatePipe,
+  ],
   templateUrl: './topbar.component.html',
   styleUrl: './topbar.component.css',
 })
 export class TopbarComponent implements OnInit {
-
   isOnline = true;
   currentDate: Date;
   utc: string;
